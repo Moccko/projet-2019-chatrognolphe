e53acd9c71b54fede61a15678c0303f73f260bf8
Roman Rieunier <rieunierroman@hotmail.fr>
2019-03-08 04:23:05 +0100

    v0.0.4 - Retrieving correctly the user with authentication + added "forgot password" screen

--- screens/SignIn.js
+++ screens/SignIn.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, TouchableOpacity, Image, Alert, Text, TextInput, StyleSheet} from 'react-native';
+import {View, TouchableOpacity, Image, Alert, Text, TextInput, KeyboardAvoidingView, StyleSheet} from 'react-native';
 
 import DB from '../data/Database';
 
@@ -7,10 +7,9 @@ import md5 from 'crypto-js/md5';
 
 export default class SignIn extends React.Component {
 
-    state = {
-        username: '',
-        password: ''
-    };
+    username = "";
+    password = "";
+    pwdInput = undefined;
 
     componentDidMount() {
         console.log("hey hey", md5("hey hey").toString());
@@ -22,8 +21,25 @@ export default class SignIn extends React.Component {
     };
 
     _signIn = () => {
-        console.log('clic');
-        // DB.collection('users');
+        DB.collection('users')
+            .where('nickname', '==', this.username)
+            .where('pwd', '==', this.password)
+            .get()
+            .then(snapshot => {
+                if (!snapshot.empty)
+                    snapshot.forEach(u => console.log(u.data()));
+                else
+                    DB.collection('users')
+                        .where('email', '==', this.username)
+                        .where('pwd', '==', this.password)
+                        .get()
+                        .then(snapshot => {
+                            if (!snapshot.empty)
+                                snapshot.forEach(u => console.log(u.data()));
+                            else
+                                Alert.alert("Erreur", "Utilisateur introuvable");
+                        });
+            });
     };
 
     _signUp = () => {
@@ -33,25 +49,34 @@ export default class SignIn extends React.Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text style={styles.title}>Connexion</Text>
-                <Image source={{uri: "https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"}} style={styles.image}/>
-                <Text style={styles.label}>Pseudo / email / téléphone</Text>
-                <TextInput style={styles.input} value={this.state.username} onEndEditing={this._signIn}/>
+                <KeyboardAvoidingView behavior="position">
+                    <Text style={styles.title}>Connexion</Text>
+                    <Image source={{uri: "https://media.giphy.com/media/o0vwzuFwCGAFO/giphy.gif"}}
+                           style={styles.image}/>
+                    <Text style={styles.label}>Pseudo / email</Text>
+                    <TextInput style={styles.input} onChangeText={text => this.username = text.toLowerCase()}
+                               returnKeyType="next" onEndEditing={() => this.pwdInput.focus()} autoCapitalize="none"
+                               keyboardType="email-address"/>
+                    {/*onEndEditing={this._signIn} textContentType="username" returnKeyType="next"/>*/}
 
-                <Text style={styles.label}>Mot de passe</Text>
-                <TextInput style={styles.input} value={this.state.password} onEndEditing={this._signIn}/>
+                    <Text style={styles.label}>Mot de passe</Text>
+                    <TextInput style={styles.input} onChangeText={text => this.password = md5(text).toString()}
+                               secureTextEntry onEndEditing={this._signIn} returnKeyType="go"
+                               ref={ref => this.pwdInput = ref}/>
+                    {/*onEndEditing={this._signIn} textContentType="password" returnKeyType="go"/>*/}
 
-                <TouchableOpacity onPress={this._forgotPwd}>
-                    <Text style={styles.forgotPwd}>Mot de passe oublié ?</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity onPress={this._forgotPwd}>
+                        <Text style={styles.forgotPwd}>Mot de passe oublié ?</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity style={styles.signIn} onPress={this._signIn}>
-                    <Text style={styles.signInLabel}>CONNEXION</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.signIn} onPress={this._signIn}>
+                        <Text style={styles.signInLabel}>CONNEXION</Text>
+                    </TouchableOpacity>
 
-                <TouchableOpacity style={styles.signUp} onPress={this._signUp}>
-                    <Text style={styles.signUpLabel}>S'INSCRIRE</Text>
-                </TouchableOpacity>
+                    <TouchableOpacity style={styles.signUp} onPress={this._signUp}>
+                        <Text style={styles.signUpLabel}>S'INSCRIRE</Text>
+                    </TouchableOpacity>
+                </KeyboardAvoidingView>
             </View>
         );
     }
